e01014a746a5411f51f43290bdbeb87e0dae919c
Darren Tucker <thesecretspotsurfapp@gmail.com>
2020-05-11 23:10:57 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by DJ on 5/11/20.
+//  Copyright © 2020 DJ Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
